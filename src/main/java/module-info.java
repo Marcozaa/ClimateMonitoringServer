35193e5cfccbd9be6e4f43983ce19b8fc3fe2de7
MoaFdl <100467256+MoaFdl@users.noreply.github.com>
2024-08-26 15:17:51 +0200

    modifiche

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.climatemonitoringserver {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens com.example.climatemonitoringserver to javafx.fxml;
     exports com.example.climatemonitoringserver;
